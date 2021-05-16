075d1912860bea975edeb933f2b56ccedf2c194b
ChristopherGustafson <christopher.gustafson@outlook.com>
2021-05-16 19:40:12 +0200

    Added votekick command

--- index.js
+++ index.js
@@ -15,6 +15,36 @@ bot.on('message', msg => {
 
   if (msg.guild !== "undefined" && msg.guild) {
 
+    // Check for votekick command
+    if(msg.content.includes("!votekick") && msg.mentions.members.first().nickname) {
+      
+      const victim = msg.mentions.members.first();
+      msg.reply("Votekick " + victim.displayName + "?").then(function (message) {
+
+        // Check how many votes are needed, set to majority of how many people is in the voice channel
+        const votesNeeded = (msg.member.voice.channel.members.size/2)+1;
+ 
+        // React to show voting reaction options
+        message.react("👍");
+        message.react("👎");
+        // Filter on thumbs up
+        const filter = (reaction, user) => reaction.emoji.name === '👍';
+        const collector = message.createReactionCollector(filter, { time: 15000 });
+        collector.on('end', collected => {
+          console.log(collected.get('👍').count);
+          if(collected.get('👍').count > votesNeeded){
+            // Send to aids
+            msg.mentions.members.first().voice.setChannel("595337249063960577");
+            msg.reply("He outie");
+          }
+          else{
+            msg.reply("Not enough votes :(");
+          }
+        });
+        }).catch(function() {
+          console.log("Error replying to message");
+      });
+    }
 
     if (msg.mentions.has(bot.user)) {
       let index = Math.floor(Math.random() * mentionReplies.length);
@@ -26,6 +56,11 @@ bot.on('message', msg => {
   let voiceChannel;
   switch (msg.content) {
 
+    case "ping":
+      msg.channel.send("fak u", { tts: true });
+      break;
+
+    // Command for bot to leave channel
     case "!stop":
       voiceChannel = bot.voice.connections.first().channel;
       if(voiceChannel) {
@@ -34,12 +69,10 @@ bot.on('message', msg => {
       }
       break;
 
+
+      // Command to show all possible sound commands
     case "!commands":
       msg.channel.send("Possible commands: " + soundCommands.join(", "), { tts: true });
-      break;
-
-    case "ping":
-      msg.channel.send("fak u", { tts: true });
       break;
 
     case "<:joellol:693565923579134022>":
